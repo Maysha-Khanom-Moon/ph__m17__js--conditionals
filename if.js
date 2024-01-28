ec069ddd3b729c9bbfc6595028af90b7fd2ca821
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 20:39:35 +0600

    only if and if-else

---
+++ if.js
@@ -0,0 +1,31 @@
+/**
+ * ----------- STRUCTURE -----------
+ * if(codition) {
+ *      // condition ture
+ * }
+ * 
+ * 
+ * ---------------------------------
+ * if(condition) {
+ *      // condition true
+ * } 
+ * else {
+ *      // condition false
+ * }
+ * 
+*/
+
+// 1. only if
+if(3 < 10) {
+    console.log('I have smaller value');
+}
+
+// 2. if-else
+var biryaniPrice = 100;
+
+if(biryaniPrice < 100) {
+    console.log('Give me some biryani');
+}
+else {
+    console.log('Give me some Rice and Dal');
+}
