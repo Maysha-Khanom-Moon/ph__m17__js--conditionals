c6b1903eaf61f844263ea68b126aeee67ef2ef93
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 20:46:16 +0600

    multiple condition using via &&, ||

---
+++ multiple.js
@@ -0,0 +1,30 @@
+// const: it can't be changed
+
+const salary = 25000;
+const isBCS = true;
+
+if(salary > 20000) {
+    console.log("Shu___patro");
+}
+else {
+    console.log('onno patro khuji');
+}
+
+
+/* -------multiple condition------- */
+ 
+// or: ||
+if(salary > 25000 || isBCS) {
+    console.log('eso baba kobul');
+}
+else {
+    console.log('vaag tui mokgul');
+}
+
+// and: &&
+if(salary > 25000 && isBCS) {
+    console.log('pola toh aguner gola');
+}
+else {
+    console.log('ami raji nah');
+}
