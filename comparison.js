d7e838a7b4f6e2d505ca58966c6590036971869d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 20:33:01 +0600

    comparison operators using

---
+++ comparison.js
@@ -0,0 +1,18 @@
+console.log(5 > 10); // false
+console.log(5 < 10); // true
+console.log(5 == 10); // false
+console.log(10 == 10); // true
+console.log(15 >= 10); // true
+console.log(15 <= 10); // false
+console.log(10.1 == 10); // false
+console.log(10.1 != 10); // true
+
+
+
+/**
+ * == : just check the value
+ * === : check value and type both
+ */
+
+console.log(10 == '10'); // true
+console.log(10 === '10'); // false
