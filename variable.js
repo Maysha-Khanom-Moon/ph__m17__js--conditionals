8d43f4757eb005cfdd9f32137024f77e5931e26c
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 20:20:50 +0600

    recape of previous module

---
+++ variable.js
@@ -0,0 +1,30 @@
+// variable ==> container
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = 'EEE';
+
+// boolean
+var passed = true;
+
+/**
+ * Naming convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
